f98e8543cacdb42168d571599157fc83bbdc8147
botnaysard <botnaysard@protonmail.com>
2018-02-01 11:40:25 -0500

    switched to previous mongodb version for tutorial purposes

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require ('body-parser')
+const MongoClient = require('mongodb').MongoClient
+var db
 
-app.listen(3000, function() {
-	console.log('listening on port 3000.')
+
+/* start the server */
+
+MongoClient.connect("mongodb://test:test@ds221258.mlab.com:21258/nodetest-db", (err, database) => {
+  	if (err) return console.log(err)
+  	db = database
+  	app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
 })
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+
 
 /* code block written in plain JS
 
@@ -27,4 +42,14 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 })
 
-// RESUME TUTORIAL AT INSTALLING NODEMON 
+/* when form is submitted */
+
+app.post('/quotes', (req, res) => {
+  	db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+//  RESUME @ SHOWING QUOTES TO USERS //
