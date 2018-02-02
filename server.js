a124090b7516a4aff4b9f01ed78316d256ba2e2e
botnaysard <botnaysard@protonmail.com>
2018-02-02 11:08:54 -0500

    final version

--- server.js
+++ server.js
@@ -1,22 +1,60 @@
-const express = require('express');
-const app = express();
-const bodyParser = require ('body-parser')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+
 var db
 
-
-/* start the server */
-
-MongoClient.connect("mongodb://test:test@ds221258.mlab.com:21258/nodetest-db", (err, database) => {
-  	if (err) return console.log(err)
-  	db = database
-  	app.listen(3000, () => {
+MongoClient.connect('mongodb://test:test@ds221258.mlab.com:21258/nodetest-db', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(process.env.PORT || 3000, () => {
     console.log('listening on 3000')
   })
 })
 
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndDelete({name: req.body.name}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('A darth vadar quote got deleted')
+  })
+})
 
 
 
@@ -36,20 +74,10 @@ app.get('/', (req, res) => {
 
 */
 
-/* send a file instead */
+/* send a file instead
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 })
 
-/* when form is submitted */
-
-app.post('/quotes', (req, res) => {
-  	db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-
-//  RESUME @ SHOWING QUOTES TO USERS //
+*/
